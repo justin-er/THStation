e382b76b3f8aab2656abac7d59147f017fe30ba3
Amir Eghtedari <eghtedaria.amir@gmail.com>
2023-12-28 01:21:38 +0100

    Move App folder to main to get rid of hader including issues.

---
+++ main/App/THReader/THReader.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <dht.h>
+
+void dht_test(void *pvParameters)
+{
+    float temperature, humidity;
+
+#ifdef CONFIG_EXAMPLE_INTERNAL_PULLUP
+    gpio_set_pull_mode(dht_gpio, GPIO_PULLUP_ONLY);
+#endif
+
+    while (1)
+    {
+        if (dht_read_float_data(DHT_TYPE_AM2301, 17, &humidity, &temperature) == ESP_OK)
+            printf("Humidity: %.1f%% Temp: %.1fC\n", humidity, temperature);
+        else
+            printf("Could not read data from sensor\n");
+
+        // If you read the sensor data too often, it will heat up
+        // http://www.kandrsmith.org/RJS/Misc/Hygrometers/dht_sht_how_fast.html
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
--- main/main.c
+++ main/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+#include "App/THReader/THReader.h"
+
 void app_main(void)
 {
-
+    // xTaskCreate(dht_test, "dht_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }
